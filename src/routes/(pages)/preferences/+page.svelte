bd1d1b4e56896dc0583a6b575d01c36ed5bdeda3
Andreas Olsson <noname.olsson@gmail.com>
2023-12-15 09:51:11 +0100

    Add new note page and header (#10)
    add new note page and header

--- src/routes/(pages)/preferences/+page.svelte
+++ src/routes/(pages)/preferences/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { pageTitle } from '$lib/stores';
 	import { DarkMode } from 'flowbite-svelte';
-
+	import '../../../app.pcss';
 	$pageTitle = 'Preferences';
 </script>
 
